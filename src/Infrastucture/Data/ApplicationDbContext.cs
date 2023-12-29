61604b552090c529438f47456467e371afd57837
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2023-12-29 13:34:26 +0500

    Implement application db context

---
+++ src/Infrastucture/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Stride.Application.Common.Interfaces;
+using Stride.Domain.Entities;
+using Stride.Infrastucture.Identity;
+using System.Reflection;
+
+namespace Stride.Infrastucture.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options), IApplicationDbContext
+{
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    public DbSet<TodoList> TodoLists => Set<TodoList>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
