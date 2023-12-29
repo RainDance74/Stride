b78a864c89f4da6c9c873c1b59721377846c3764
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2023-12-29 13:32:44 +0500

    Create manageable entity class

---
+++ src/Domain/Common/BaseManageableEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Stride.Domain.Common;
+
+public class BaseManageableEntity : BaseAuditableEntity<int>
+{
+    public string CreatedBy { get; set; } = null!;
+    public string? UpdatedBy { get; set; }
+}
