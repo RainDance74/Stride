8f104ae4dd8b46b61a10f2a767cca07902e8859c
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2023-12-29 13:28:21 +0500

    Add DbContext contract to Application

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stride.Domain.Entities;
+
+namespace Stride.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<TodoItem> TodoItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
