6ac153d28caf83cc92633a3bd119bd9750e1d9c3
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2023-12-30 22:36:52 +0500

    Remove top directive statements

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,26 +1,20 @@
-﻿internal class Program
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddWebServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddInfrastructureServices(builder.Configuration);
-        builder.Services.AddWebServices();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if(app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.MapEndpoints();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.MapEndpoints();
+
+app.Run();
