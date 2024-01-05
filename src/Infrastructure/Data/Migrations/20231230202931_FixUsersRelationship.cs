b641c15f769f7e794e95ead250451e3103be1670
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-01-05 05:38:26 +0500

    Fix triggers in migration

--- src/Infrastructure/Data/Migrations/20231230202931_FixUsersRelationship.cs
+++ src/Infrastructure/Data/Migrations/20231230202931_FixUsersRelationship.cs
@@ -9,16 +9,60 @@ namespace Stride.Infrastructure.Data.Migrations
     {
         private const string _addInsertUserTriggerCommand =
         """
+            CREATE OR REPLACE FUNCTION stride_users_after_insert()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                INSERT INTO public.stride_users (id, created_date_time)
+                VALUES (NEW.id, NOW());
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
             CREATE TRIGGER stride_users_after_insert
-            AFTER INSERT ON public.AspNetUsers
-            INSERT INTO public.stride_users (id)
-            VALUES (NEW.id);
+            AFTER INSERT ON public."AspNetUsers"
+            FOR EACH ROW EXECUTE PROCEDURE stride_users_after_insert();
         """;
 
         private const string _dropInsertUserTriggerCommand =
         """
             DROP TRIGGER stride_users_after_insert
-            ON public.AspNetUsers
+            ON public."AspNetUsers";
+
+            DROP FUNCTION stride_users_after_insert();
+        """;
+
+        private const string _addUpdateUserTriggerCommand =
+        """
+            CREATE OR REPLACE FUNCTION stride_users_updated_at_trigger()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                IF TG_TABLE_NAME = 'AspNetUsers' THEN
+                    UPDATE public.stride_users SET updated_date_time = NOW() WHERE id = NEW.id;
+                ELSE
+                    NEW.updated_date_time = NOW();
+                END IF;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER stride_users_updated_at_trigger
+            AFTER UPDATE ON public."AspNetUsers"
+            FOR EACH ROW EXECUTE PROCEDURE stride_users_updated_at_trigger();
+
+            CREATE TRIGGER stride_users_updated_at_trigger
+            AFTER UPDATE ON public.stride_users
+            FOR EACH ROW EXECUTE PROCEDURE stride_users_updated_at_trigger();
+        """;
+
+        private const string _dropUpdateUserTriggerCommand =
+        """
+            DROP TRIGGER stride_users_updated_at_trigger
+            ON public."AspNetUsers";
+
+            DROP TRIGGER stride_users_updated_at_trigger
+            ON public.stride_users;
+
+            DROP FUNCTION stride_users_updated_at_trigger();
         """;
 
         /// <inheritdoc />
@@ -37,6 +81,7 @@ namespace Stride.Infrastructure.Data.Migrations
                 onDelete: ReferentialAction.Cascade);
 
             migrationBuilder.Sql(_addInsertUserTriggerCommand);
+            migrationBuilder.Sql(_addUpdateUserTriggerCommand);
         }
 
         /// <inheritdoc />
@@ -55,6 +100,7 @@ namespace Stride.Infrastructure.Data.Migrations
                 onDelete: ReferentialAction.Cascade);
 
             migrationBuilder.Sql(_dropInsertUserTriggerCommand);
+            migrationBuilder.Sql(_dropUpdateUserTriggerCommand);
         }
     }
 }
