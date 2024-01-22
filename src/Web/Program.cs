61cdaa8be856a9b7b8edb7c0ae65b7fd720c510a
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-01-22 06:23:02 +0500

    Add hsts if not development

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -17,6 +17,11 @@ if(app.Environment.IsDevelopment())
 {
     await app.InitialiseDatabaseAsync();
 }
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
