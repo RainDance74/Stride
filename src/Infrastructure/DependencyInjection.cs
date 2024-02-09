f1bb9ea5f55988691054f5005efdcd042faa4192
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-02-09 14:49:04 +0500

    Fix cookie based authentication

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 
 using Stride.Application.Common.Interfaces;
 using Stride.Infrastructure.Data;
@@ -36,20 +37,32 @@ public static class DependencyInjection
 
         services.AddScoped<ApplicationDbContextInitialiser>();
 
-        services.AddAuthentication(opt =>
-        {
-            opt.DefaultAuthenticateScheme = IdentityConstants.BearerScheme;
-            opt.DefaultChallengeScheme = IdentityConstants.BearerScheme;
-            opt.DefaultScheme = IdentityConstants.BearerScheme;
-        })
+        services
+            .AddAuthentication(opt =>
+            {
+                opt.DefaultScheme = "BEARER_OR_COOKIE";
+                opt.DefaultChallengeScheme = "BEARER_OR_COOKIE";
+            })
             .AddBearerToken(IdentityConstants.BearerScheme)
             .AddCookie("Identity.Application", opt =>
             {
                 opt.ExpireTimeSpan = TimeSpan.FromDays(7);
                 opt.SlidingExpiration = true;
+                opt.Cookie.Name = "Stride.Bearer";
                 opt.Cookie.HttpOnly = true;
                 opt.LoginPath = "/login";
+            })
+            .AddPolicyScheme("BEARER_OR_COOKIE", "BEARER_OR_COOKIE", options =>
+            {
+                options.ForwardDefaultSelector = context =>
+                {
+                    string? authorization = context.Request.Headers[HeaderNames.Authorization];
+                    return !string.IsNullOrEmpty(authorization) && authorization.StartsWith("Bearer ")
+                        ? IdentityConstants.BearerScheme
+                        : "Identity.Application";
+                };
             });
+        ;
 
         services.AddAuthorizationBuilder();
 
